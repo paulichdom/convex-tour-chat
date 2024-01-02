f6d956a5bfd51f34d8672a2dda15cd2e07308786
Domagoj <paulic.domagoj@gmail.com>
2024-01-02 11:21:34 +0100

    add chat action

--- convex/openai.ts
+++ convex/openai.ts
@@ -1,5 +1,14 @@
 'use node';
 import OpenAI from 'openai';
+import { action } from './_generated/server';
+import { v } from 'convex/values';
 
 const apiKey = process.env.OPENAI_API_KEY!;
 const openai = new OpenAI({ apiKey });
+
+export const chat = action({
+  args: {
+    messageBody: v.string(),
+  },
+  handler: async (ctx, args) => {},
+});
