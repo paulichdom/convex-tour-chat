1c14dd1d83e1b92ec5f5997275ed3e3f79e3b542
Domagoj <paulic.domagoj@gmail.com>
2024-01-02 11:25:26 +0100

    get response from a handler

--- convex/openai.ts
+++ convex/openai.ts
@@ -10,5 +10,22 @@ export const chat = action({
   args: {
     messageBody: v.string(),
   },
-  handler: async (ctx, args) => {},
+  handler: async (ctx, args) => {
+    const response = await openai.chat.completions.create({
+      model: 'gpt-3.5-turbo',
+      messages: [
+        {
+          role: 'system',
+          content:
+            'You are a terse bot in a group chat responding to questions with 1-sentence answers.',
+        },
+        {
+          role: 'user',
+          content: args.messageBody,
+        },
+      ],
+    });
+
+    const messageContent = response.choices[0].message?.content;
+  },
 });
